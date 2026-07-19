ded01f756f588aa502843a139019ed6d993ef801
pasokoone <a0pasokon@gmail.com>
2026-07-20 01:12:35 +0900

    chore: remove legacy component files

--- components/componentRegistry.ts
+++
@@ -1,9 +0,0 @@
-import { AtticCopy, CrtCopy, FridgeCopy, LandingCopy, StairsCopy } from "./StageComponents";
-
-export const componentRegistry = {
-  stairs: StairsCopy,
-  landing: LandingCopy,
-  attic: AtticCopy,
-  fridge: FridgeCopy,
-  crt: CrtCopy,
-} as const;
