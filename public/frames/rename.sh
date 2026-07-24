2ae964d2b0f15d44accf5619ab3d95c84be05a1d
pasokoone <a0pasokon@gmail.com>
2026-07-24 14:17:00 +0900

    feat(scene): add blue fridge metro station kotatsu and scrolling distance adjustment

---
+++ public/frames/rename.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+reset_sequence() {
+    dir="$1"
+
+    # 一旦 tmp_ を付ける
+    for file in "$dir"/*.webp; do
+        mv "$file" "$dir/tmp_$(basename "$file")"
+    done
+
+    # 0001から振り直す
+    i=1
+    for file in $(find "$dir" -maxdepth 1 -name 'tmp_*.webp' | sort); do
+        printf -v newname "%04d.webp" "$i"
+        mv "$file" "$dir/$newname"
+        ((i++))
+    done
+}
+
+reset_sequence "fridge"
